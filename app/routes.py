008e1b3932897566a86973c67894c000daea1299
davy1ex <dezy1ex@gmail.com>
2019-09-02 05:50:42 +0400

    add ajax request

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,10 @@ from app import app
 
 @app.route("/")
 def index():
-    temp = requests.get("http://192.168.0.87").text.split("temp")[1][:5]
-
-    # html
-
-# >>> html.split("temp")[1]
-# ':24.00\r\n</body>\r\n</html>\r\n'
-# >>> html.split("temp")[1][5:]
-# '0\r\n</body>\r\n</html>\r\n'
-# >>> html.split("temp")[1][:5]
+    temp = requests.get("http://192.168.0.87").text.split("temp")[1][1:5]
     return render_template("index.html", temp=temp)
+
+@app.route("/temp")
+def temp():
+    temp = requests.get("http://192.168.0.87").text.split("temp")[1][1:5]
+    return temp
